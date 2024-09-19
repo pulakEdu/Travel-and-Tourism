bb0bd26661fca8262490a33a42346ad6ff76dd53
pulakEdu <educarefrompulak.com>
2024-09-19 14:04:25 +0530

    first commit

---
+++ Front.js
@@ -0,0 +1,33 @@
+var crsr = document.querySelector("#cursor")
+document.addEventListener("mousemove", function(dets){
+    //crsr.style.top = dets.y+"px"
+
+})
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        //markers: true,
+        start: "top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        //markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
